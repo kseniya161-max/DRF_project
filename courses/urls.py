36337730ecb7d89bdd261e48c9d0f5088e4d5dc5
Kseniya <baharevaxen@yandex.ru>
2025-11-28 15:16:03 +0300

    create urls for Lesson+CRUD

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
-from courses.views import CourseViewSet
+from courses.views import CourseViewSet, LessonListAPIView, LessonCreateAPIView, LessonUpdateAPIView, \
+    LessonRetrieveAPIView, LessonDestroyAPIView
 
 app_name = "courses"
 
@@ -7,5 +9,11 @@ router = SimpleRouter()
 router.register(r"course", CourseViewSet)
 
 
-urlpatterns = []
+urlpatterns = [
+    path('lesson/',LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/create/',LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/<int:pk>/update/',LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/<int:pk>/',LessonRetrieveAPIView.as_view(), name='lesson_retrieve'),
+    path('lesson/<int:pk>/delete/',LessonDestroyAPIView.as_view(), name='lesson_delete'),
+]
 urlpatterns += router.urls
