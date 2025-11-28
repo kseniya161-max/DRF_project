5c321263fcf4f181583116d96edc38e66adf32e7
Kseniya <baharevaxen@yandex.ru>
2025-11-28 12:54:08 +0300

    create model User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    " Модель Пользователя"
+
+    username = None
+    email = models.EmailField(max_length=35, unique=True, help_text="Введите email")
+    phone = models.CharField(
+        max_length=35, blank=True, null=True, help_text="Введите номер телефона"
+    )
+    location = models.CharField(
+        max_length=50, blank=True, null=True, help_text="Введите город"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar", blank=True, null=True, help_text="Загрузите аватар"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
