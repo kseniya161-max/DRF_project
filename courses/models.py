291f1888923cfa3e34fb3cd405bc045604310be0
Kseniya <baharevaxen@yandex.ru>
2025-11-28 13:07:23 +0300

    create model Course

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    "Модель Курса"
+    name = models.CharField(max_length=150, unique=True, help_text='Название')
+    preview = models.ImageField(upload_to='courses/images', help_text='Изображение')
+    description = models.TextField(max_length=300, blank=True, null=True, help_text='Введите описание')
+
+    def __str__(self):
+        return self.name
+
+
+class Meta:
+    verbose_name = "Курс"
+    verbose_name_plural = "Курсы"
--- users/models.py
+++ users/models.py
@@ -19,3 +19,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class Meta:
+    verbose_name = 'Пользователь'
+    verbose_name_plural = 'Пользователи'
