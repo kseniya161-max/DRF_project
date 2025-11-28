15c61b0e0f310cf27445efca9c2efbd882ded843
Kseniya <baharevaxen@yandex.ru>
2025-11-28 14:43:28 +0300

    create serializer+router

---
+++ courses/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import SimpleRouter
+from courses.views import CourseViewSet
+
+app_name = 'courses'
+
+router = SimpleRouter()
+router.register(r'course', CourseViewSet)
+
+
+urlpatterns = []
+urlpatterns += router.urls
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from courses.models import Course
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    
+
