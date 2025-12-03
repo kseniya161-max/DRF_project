852ffa1e3b33d387b2b8efa5532a198c6243fe17
Kseniya <baharevaxen@yandex.ru>
2025-12-03 10:07:51 +0300

    info for lesson in course (correcting serializer)

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,25 +3,27 @@ from rest_framework.serializers import ModelSerializer
 
 from courses.models import Course, Lesson
 
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 
 class CourseSerializer(ModelSerializer):
+    lesson = LessonSerializer()
     class Meta:
         model = Course
         fields = "__all__"
 
 
+
 class CourseSerializerDetail(ModelSerializer):
     count_course_with_same_lesson = SerializerMethodField()
+    lesson = LessonSerializer()
 
     def get_count_course_with_same_lesson(self, course):
         return Course.objects.filter(lesson=course.lesson).count()
     class Meta:
         model = Course
         fields = ('name', 'preview', 'description', 'count_course_with_same_lesson')
-
-
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
