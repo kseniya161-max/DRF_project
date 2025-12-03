036cbefe75d85b08326beb4466494eb1b82a95fc
Kseniya <baharevaxen@yandex.ru>
2025-12-03 11:18:22 +0300

    correcting flake8

--- config/settings.py
+++ config/settings.py
@@ -17,11 +17,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-import os
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
