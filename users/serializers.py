b8072785081e6be1628bc8ccd510668c1b7d9ae9
Kseniya <baharevaxen@yandex.ru>
2025-12-04 12:39:16 +0300

    adding checkout to existing email

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 
 from users.models import Payments, User
@@ -14,4 +15,9 @@ class UserAPIView(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise ValidationError("Данный email уже существует")
+        return value
 
