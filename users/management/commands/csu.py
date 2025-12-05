3741a15a74d9613b0b6f8b1215d45c8883ef5239
Kseniya <baharevaxen@yandex.ru>
2025-12-05 09:55:34 +0300

    create superuser

---
+++ users/management/commands/csu.py
@@ -0,0 +1,14 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(email='admin_1@mail.ru')
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.set_password('qwe123')
+        user.save()
+
---
+++ users/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework import permissions
+
+class CustomerAccessPermission(permissions.BasePermission):
+    message = 'Adding customers not allowed.'
+
+    def has_permission(self, request, view):
