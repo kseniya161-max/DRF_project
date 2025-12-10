5f8ac586c92d9b29848c96444ceef7bee9e91c93
Kseniya <baharevaxen@yandex.ru>
2025-12-10 12:25:36 +0300

    create test

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from courses.models import Lesson, Course
+from users.models import User
+
+
+class LessonSubscriptionTest(APITestCase):
+    def setUp(self):
+        """Создаем значения"""
+        self.user = User.objects.create(email="admin_3@mail.ru")
+        self.lesson = Lesson.objects.create(title="Elementary")
+        self.course = Course.objects.create(name="Английский", owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+
+
