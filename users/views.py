79378f1ab0685795933d1a49f6a4ba484161a091
Kseniya <baharevaxen@yandex.ru>
2025-12-20 17:28:25 +0300

    adding documentation to endpoint for users

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import filters
 from rest_framework.generics import (
     ListAPIView,
@@ -27,11 +28,25 @@ class PaymentsListAPIView(ListAPIView):
     ordering_fields = ("payment_date",)
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        operation_description="Retrieve a list of payments",
+        responses={200: PaymentsSerializer(many=True)}
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
 
 class UserListAPIView(ListAPIView):
     serializer_class = UserAPIView
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        operation_description="Retrieve a list of users",
+        responses={200: UserAPIView(many=True)}
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 
 class UserRetrieveAPIView(RetrieveAPIView):
@@ -39,11 +54,25 @@ class UserRetrieveAPIView(RetrieveAPIView):
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        operation_description="Retrieve specific user",
+        responses={200: UserAPIView}
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
 
 class UserDestroyAPIView(DestroyAPIView):
     serializer_class = UserAPIView
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        operation_description="Destroy a list of users",
+        responses={204: 'No Content'}
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
 
 
 class UserUpdateAPIView(UpdateAPIView):
@@ -51,10 +80,26 @@ class UserUpdateAPIView(UpdateAPIView):
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        operation_description="Update a specific user",
+        request_body=UserAPIView,
+        responses={200: UserAPIView}
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserAPIView
     queryset = User.objects.all()
+
+    @swagger_auto_schema(
+        operation_description="Create a new user",
+        request_body=UserAPIView,
+        responses={201: UserAPIView}
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
