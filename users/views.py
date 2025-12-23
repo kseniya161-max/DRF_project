0774fe6b6e34b35b5a4502b636312533f4862a7d
Kseniya <baharevaxen@yandex.ru>
2025-12-23 19:32:19 +0300

    view for payment

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg.openapi import Response
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import filters
+from rest_framework import filters, status
 from rest_framework.generics import (
     ListAPIView,
     CreateAPIView,
@@ -10,12 +11,19 @@ from rest_framework.generics import (
     UpdateAPIView,
 )
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
+from django.conf import settings
+
+from courses.models import Course
 from users.models import Payments, User
 from users.serializers import PaymentsSerializer, UserAPIView
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-# Create your views here.
+
 class PaymentsListAPIView(ListAPIView):
     queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
@@ -35,6 +43,63 @@ class PaymentsListAPIView(ListAPIView):
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
+
+class CreatePaymentAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request):
+        user = request.user
+        course_id = request.data.get('course_id')
+        amount = request.data.get('amount')
+
+        try:
+            course = Course.objects.get(id=course_id)
+        except Course.DoesNotExist:
+            return Response({'error': 'Course not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        product = stripe.Product.create(name=course.name)
+
+        price = stripe.Price.create(
+            unit_amount=int(amount * 100),
+            currency='usd',
+            product=product.id,
+        )
+
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price': price.id,
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url='https://yourdomain.com/success/',
+            cancel_url='https://yourdomain.com/cancel/',
+        )
+
+        payment = Payments.objects.create(
+            username=user,
+            paid_course=course,
+            sum=amount,
+            payment_detail='Stripe Payment',
+        )
+
+        return Response({'url': session.url}, status=status.HTTP_200_OK)
+
+
+class PaymentSuccessAPIView(APIView):
+    """Успешный платеж"""
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": "Payment was successful!"}
+
+
+class PaymentFailedAPIView(APIView):
+    """Неуспешный платеж"""
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": "Failed"})
 
 class UserListAPIView(ListAPIView):
     serializer_class = UserAPIView
