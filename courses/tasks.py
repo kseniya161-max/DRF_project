de29b046368ae3be315da685860e6e935ef557d1
Kseniya <baharevaxen@yandex.ru>
2025-12-29 16:38:48 +0300

    correcting task

--- courses/tasks.py
+++ courses/tasks.py
@@ -2,7 +2,9 @@
 from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 
+from courses.models import Course, Subscription
 
 
 @shared_task
@@ -10,6 +12,7 @@ def send_information(email):
     print(f"Получен email для отправки: {email}")
     if not email:
         raise ValueError("Email не может быть пустым")
+
     send_mail(
         'Новая подписка',
         'Вы обновили подписку',
