4fd17c06f85aba318708524a22fe0ff64f9b89e0
Kseniya <baharevaxen@yandex.ru>
2025-12-30 16:12:13 +0300

    correcting users/tasks(beat)

--- users/tasks.py
+++ users/tasks.py
@@ -2,14 +2,13 @@ from celery import shared_task
 from django.utils import timezone
 from datetime import timedelta
 from users.models import User
+from django.utils import timezone
 
 
 @shared_task
 def check_user_activity():
     print("Задача check_user_activity запущена.")
-    from users.models import User
-    now = timezone.now()
-    inactive_users = User.objects.filter(last_login__lt=now - timezone.timedelta(days=30), is_active=True)
+    inactive_users = User.objects.filter(last_login__lt=timezone.now() - timedelta(days=30), is_active=True)
 
     for user in inactive_users:
         user.is_active = False
